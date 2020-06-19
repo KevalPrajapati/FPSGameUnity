5758e8d5ac15bfdd20246230c93def7e42459fad
KevalPrajapati <keval.prajapatimus@gmail.com>
2020-06-19 22:55:17 +0530

    initial commit with Movement

---
+++ Assets/Bulletshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletshot : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector3 input;
+    private Rigidbody r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump`")) {
+            input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            r.AddForce(input * Time.deltaTime * moveSpeed/100000);
+        }
+
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float jumph = 3000f;
+    // [SerializeField]
+    private float lookSensitivity = 20f;
+
+    private Rigidbody rb;
+
+    private PlayerMotor motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        rb = GetComponent<Rigidbody>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float _xMov = Input.GetAxisRaw("Horizontal");
+       float _zMov = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		// Final movement vector
+		Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+        //apply movement
+        motor.Move(_velocity);
+        //Calculate rotation as a 3D vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+        //Calculate camera rotation as a 3D vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot*lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(_cameraRotationX);
+
+
+        //Jump
+        if (Input.GetButton("Jump"))
+        {
+            rb.AddForce(0, jumph * Time.deltaTime, 0 ); 
+        }                
+
+
+
+    }
+}
